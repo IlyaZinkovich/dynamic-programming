0f43f827b775823d23950d4c1a7af085ba8ef074
IlyaZinkovich <ilya.zinkovich@gmail.com>
2020-02-14 13:52:54 +0100

    Added recursive formula for Dice Throw Problem

--- src/main/java/io/github/ilyazinkovich/dp/DiceThrowProblem.java
+++ src/main/java/io/github/ilyazinkovich/dp/DiceThrowProblem.java
@@ -4,10 +4,14 @@ import static org.junit.Assert.assertEquals;
 
 public class DiceThrowProblem {
 
-    public static int solve(int dices, int faces, int sum) {
+    /**
+     * combinations(1, j) = 1, j < min(sum, faces), 0 j >= min(sum, faces)
+     * combinations(i, j) = sum{combinations(i - 1, j - k), k in (1, min(j - 1, faces))}
+     */
+    public static int solve(int faces, int dices, int sum) {
         int[][] combinations = new int[dices + 1][sum + 1];
-        for (int i = 1; i <= Math.min(sum, faces); i++) {
-            combinations[1][i] = 1;
+        for (int j = 1; j <= Math.min(sum, faces); j++) {
+            combinations[1][j] = 1;
         }
         for (int i = 2; i <= dices; i++) {
             for (int j = 1; j <= sum; j++) {
@@ -24,5 +28,6 @@ public class DiceThrowProblem {
         assertEquals(2, solve(2, 2, 3));
         assertEquals(21, solve(6, 3, 8));
         assertEquals(4, solve(4, 2, 5));
+        assertEquals(6, solve(4, 3, 5));
     }
 }
